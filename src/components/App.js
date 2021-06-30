32de9dc67bd32c0fc7bf187eae2a07bf7f93141d
chelseasantos <mchelseasantos@gmail.com>
2021-06-30 10:29:14 -0600

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -16,12 +16,21 @@ function Home() {
   );
 }
 
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* write an <About> component here */}
+      {<NavBar />}
+      {<Home />}
+      {<About />}
     </div>
   );
 }
